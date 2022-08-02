fcff8d348d64597dbaeb348cb36e20138fd46380
Jonathas Trajano <106614188+trajanoms@users.noreply.github.com>
2022-08-02 14:13:36 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2600,
+    reset: true,
+})
+
+sr.reveal('.home-text', {delay:20, origin:'left'})
+sr.reveal('.about, .sevices, .portfolio, .blog, .contact, .copyright', {delay:100, origin:'bottom'})
